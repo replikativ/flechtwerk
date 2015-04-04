df5dcccbccee2e79619c8c7af94e3deef95e5827
kordano <konrad.kuehne@rocketmail.com>
2015-04-04 19:31:54 +0200

    update project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject geschichte-gorilla "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "Vega specifications for geschichte commit graph"
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/kordano/geschichte-gorilla"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
